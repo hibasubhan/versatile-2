596d7127e9c0ea02bc3f52b1d86695498b75151c
zaikhan <zaikhan571@gmail.com>
2026-07-17 09:12:44 -0700

    Updated restaurant website

---
+++ checkout.js
@@ -0,0 +1,96 @@
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const checkoutItems = document.getElementById("checkout-items");
+
+const checkoutTotal = document.getElementById("checkout-total");
+
+const finalTotal = document.getElementById("final-total");
+
+let total = 0;
+
+cart.forEach(function(item){
+
+    total += item.price * item.qty;
+
+    checkoutItems.innerHTML += `
+
+    <div class="checkout-item">
+
+        <h3>${item.name}</h3>
+
+        <p>
+
+        ${item.qty} × Rs.${item.price}
+
+        </p>
+
+    </div>
+
+    <hr>
+
+    `;
+
+});
+
+checkoutTotal.textContent = total;
+
+finalTotal.textContent = total + 150;
+
+const placeOrder = document.getElementById("place-order");
+
+placeOrder.addEventListener("click", function(){
+
+    const name = document.getElementById("customer-name").value.trim();
+
+    const phone = document.getElementById("customer-phone").value.trim();
+
+    const address = document.getElementById("customer-address").value.trim();
+
+    if(name === "" || phone === "" || address === ""){
+
+        alert("Please fill all required fields.");
+
+        return;
+
+    }
+
+    localStorage.removeItem("cart");
+
+window.location.href = "success.html";
+
+});
+
+let discount = 0;
+
+const applyPromo = document.getElementById("apply-promo");
+
+applyPromo.addEventListener("click", function(){
+
+    const code = document.getElementById("promo-code").value;
+
+    if(code === "SAVE10"){
+
+        discount = total * 0.10;
+
+    }
+
+    else if(code === "FREE50"){
+
+        discount = 50;
+
+    }
+
+    else{
+
+        alert("Invalid Promo Code");
+
+        return;
+
+    }
+
+    document.getElementById("discount-text").textContent =
+        "Discount : Rs." + discount;
+
+    finalTotal.textContent = total + 150 - discount;
+
+});
---
+++ success1.js
@@ -0,0 +1,34 @@
+const randomOrder = Math.floor(Math.random() * 900000) + 100000;
+
+document.getElementById("order-number").textContent =
+"Order No : #" + randomOrder;
+
+// Cart Empty
+/* localStorage.removeItem("cart"); */
+const steps = [
+
+document.getElementById("step1"),
+
+document.getElementById("step2"),
+
+document.getElementById("step3"),
+
+document.getElementById("step4")
+
+];
+
+let current = 0;
+
+setInterval(function(){
+
+    if(current < steps.length-1){
+
+        steps[current].classList.remove("active");
+
+        current++;
+
+        steps[current].classList.add("active");
+
+    }
+
+},5000);
